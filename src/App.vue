d9feb0d86db37ad1194dd2cd594d743c7f873101
Nicolò Fretti <nico.fretti@gmail.com>
2021-09-23 16:59:14 +0200

    Init project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page">
+    <Login />
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Login
   }
 }
 </script>
 
 <style lang="scss">
+@import "src/colors";
+@font-face {
+  font-family: Quicksand;
+  src: url("assets/fonts/Quicksand.ttf");
+}
+@font-face {
+  font-family: Quicksand-Bold;
+  src: url("assets/fonts/Quicksand-Bold.ttf");
+}
+body{
+  background: $BACKGROUND;
+  margin: 20px 40px 0;
+  display: flex;
+  flex-direction: column;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: Quicksand-Bold, sans-serif;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="navbar">
+    <p class="title">Jumm</p>
+    <div class="buttons">
+      <CartIcon w="40" h="40"/>
+      <LogOutIcon w="40" h="40"/>
+    </div>
+  </div>
+  <div>Olle</div>
+</template>
+
+<script>
+import CartIcon from 'vue-ionicons/dist/md-cart'
+import LogOutIcon from 'vue-ionicons/dist/md-log-out'
+export default {
+  name: 'Login',
+  props: ["msg"],
+  components:{
+    CartIcon,
+    LogOutIcon
+  },
+  data(){
+    return {
+      msg_2:"test"
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "src/colors";
+
+div.navbar{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background: $CONTAINER;
+  border-radius: 8px;
+  padding: 0 10px 0;
+  .buttons{
+    display:flex;
+    flex-direction: row;
+    * {
+      &:first-child{
+        margin-right: 30px;
+      }
+      &:hover{
+        cursor: pointer;
+      }
+
+    }
+  }
+
+}
+p.title{
+  font-size:40px;
+  margin:0;
+  &:first-letter{
+    color:$PRIMARY;
+  }
+  &:after {
+    content: "y";
+    color: $PRIMARY ;
+  }
+}
+</style>
