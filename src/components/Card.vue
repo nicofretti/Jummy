6c9dfaefe51058b3205c2e89abb073aac5b1e233
Nicolò Fretti <nico.fretti@gmail.com>
2021-09-25 14:56:13 +0200

    Starting home cards

---
+++ src/components/Card.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card">
+    <div class="image" v-bind:style="{background: 'url('+(this.recipe.immagine)+')'}"/>
+    <p>{{this.recipe.nome}}</p>
+    <div class="actions">
+      <button class="secondary"><Document w="40" h="40"/></button>
+      <button style="margin-left: 20px" class="primary"><Basket w="40" h="40"/></button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Basket from 'vue-ionicons/dist/ios-basket';
+import Document from 'vue-ionicons/dist/md-document';
+export default {
+  name: "Card",
+  props: ["id", "recipe"],
+  components:{
+    Document,
+    Basket
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "src/global";
+
+div.card{
+  display: flex;
+  flex-direction: column;
+  justify-items: flex-start;
+  width: 300px;
+  height: 230px;
+  background: $CONTAINER;
+  border-radius: 8px;
+  fill:$CONTAINER;
+  p{
+    font-size:22px;
+    text-align: center;
+    margin:10px 10px 10px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+}
+div.image{
+  min-height: 120px;
+  border-radius: 8px 8px 0 0;
+  background-position: center !important;
+  background-size: cover !important;
+}
+div.actions{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  button{
+    margin:0
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,58 @@
 <template>
   <div>
     <Navbar :active="true" @change="changeNavbar"/>
-    <div class="bar">
-      <div class="search">
-        <input v-model="search" placeholder="Ricerca ricetta..." type="text"/>
-        <button class="primary" style="margin:0">
-          <Search style="fill:white;" h="55" w="55"/>
+    <div class="body">
+      <div class="bar">
+        <div class="search">
+          <input v-model="search" placeholder="Ricerca ricetta..." type="text"/>
+          <button class="primary" style="margin:0">
+            <Search style="fill:white;" h="55" w="55"/>
+          </button>
+        </div>
+        <button class="primary"
+                @click="newReciple"
+                style="margin-left:20px">
+          <AddCircleOutline style="fill:white" w="30" h="30"/>
+          Nuova ricetta
         </button>
       </div>
-      <button class="primary"
-              @click="newReciple"
-              style="margin-left:20px">
-        <AddCircleOutline style="fill:white" w="30" h="30"/>
-        Nuova ricetta
-      </button>
-      {{searchReciples}}
+      <div class="reciples">
+        <Card :recipe="{
+          nome:'Pasta alla carbonara',
+          descrizione:'Pasta alla carbonara',
+          prodotti:[{nome:'Pasta',quantita:1}],
+          immagine:'https://firebasestorage.googleapis.com/v0/b/jummy-3a739.appspot.com/o/images%2FfDfXjZwEZ6GQixgbkQCL?alt=media&token=4aa9ef47-4aae-4df2-b7ec-ab1569a3d172'
+          }
+        "/>
+        <Card :recipe="{
+          nome:'Pasta alla carbonara',
+          descrizione:'Pasta alla carbonara',
+          prodotti:[{nome:'Pasta',quantita:1}],
+          immagine:'https://firebasestorage.googleapis.com/v0/b/jummy-3a739.appspot.com/o/images%2FfDfXjZwEZ6GQixgbkQCL?alt=media&token=4aa9ef47-4aae-4df2-b7ec-ab1569a3d172'
+          }
+        "/>
+        <Card :recipe="{
+          nome:'Pasta alla carbonara',
+          descrizione:'Pasta alla carbonara',
+          prodotti:[{nome:'Pasta',quantita:1}],
+          immagine:'https://firebasestorage.googleapis.com/v0/b/jummy-3a739.appspot.com/o/images%2FfDfXjZwEZ6GQixgbkQCL?alt=media&token=4aa9ef47-4aae-4df2-b7ec-ab1569a3d172'
+          }
+        "/>
+        <Card :recipe="{
+          nome:'Grits, la ricetta americana finta di prova',
+          descrizione:'Pasta alla carbonara',
+          prodotti:[{nome:'Pasta',quantita:1}],
+          immagine:'https://firebasestorage.googleapis.com/v0/b/jummy-3a739.appspot.com/o/images%2FfDfXjZwEZ6GQixgbkQCL?alt=media&token=4aa9ef47-4aae-4df2-b7ec-ab1569a3d172'
+          }
+        "/>
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
 import Navbar from "../components/Navbar";
+import Card from "../components/Card";
 import AddCircleOutline from 'vue-ionicons/dist/md-add-circle-outline';
 import Search from 'vue-ionicons/dist/md-search';
 export default {
@@ -30,7 +61,8 @@ export default {
   components: {
     Navbar,
     AddCircleOutline,
-    Search
+    Search,
+    Card
   },
   data: () => {
     return {
@@ -57,12 +89,13 @@ export default {
 <style scoped lang="scss">
 @import "src/global";
 
-
+div.body{
+  margin:40px 200px 20px;
+}
 div.bar{
   display:flex;
   justify-content: center;
   align-items:center;
-  margin: 40px 200px 20px;
   button{
     margin:0;
     font-size:20px
@@ -93,5 +126,12 @@ div.search{
   button{
     margin-left: -10px;
   }
+}
+
+div.reciples{
+  padding: 40px 0 40px;
+  display: grid;
+  grid-template-columns: repeat(3,1fr);
+  gap:20px
 }
 </style>
