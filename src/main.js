949ad8268e37a735ce25f88b4416367cdcb2275e
Frex <34170904+nicofretti@users.noreply.github.com>
2021-11-30 16:17:47 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,12 +5,7 @@ import firebase from "firebase/compat/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAELj8EWL86q6YPPGyC-z5NYm99QhIYOmU",
-    authDomain: "jummy-3a739.firebaseapp.com",
-    projectId: "jummy-3a739",
-    storageBucket: "jummy-3a739.appspot.com",
-    messagingSenderId: "509479044250",
-    appId: "1:509479044250:web:d6570f414be3d7464e0b23"
+    /*YUMMY CONFIG*/
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
